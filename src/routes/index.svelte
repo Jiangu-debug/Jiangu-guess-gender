39ee4437e5449c3aa0b27f5cb3d60b0b755bdb1c
Jiangu-debug <clhs.d5174@gmail.com>
2021-12-19 04:19:44 +0000

    commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<h1>Guess Gender</h1>
+<form on:submit|preventDefault={getName}>
+    <label for="name">Name:</label>
+    <input id="name" bind:value={name} type="text" />
+    <button type="submit">Check</button>
+</form>
+
+<script>
+    let name=''
+    let gender=''
+    async function getName() {
+  fetch('https://api.genderize.io/?name=Mickey' + name)
+	.then((response) => response.json())
+	.then((data) => {
+  	gender = data.gender; // Path for gender
+	});
+}
+</script>
+<p>Gender:{gender}</p>
